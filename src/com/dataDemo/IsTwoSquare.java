0d48281b10f0a054447f73c5b6bcf24dcecbca6c
upupuup <woyaojiayou_a@163.com>
2019-01-24 17:35:38 +0800

    快速判断一个数是否是2的乘方的方法

--- src/com/dataDemo/IsTwoSquare.java
+++ src/com/dataDemo/IsTwoSquare.java
@@ -5,35 +5,36 @@ package com.dataDemo;
  */
 public class IsTwoSquare {
     public static void main(String[] args) {
-        int num = 0b1111001;
+        int num = 0b1000;
         fun1(num);
         fun2(num);
     }
 
     /**
-     * 方法一：与1相与，然后向右移动一位
+     * 方法一,从1开始，一直乘2，然后和num匹配
+     * @param num 二进制数
      */
     private static void fun1(int num) {
-        System.out.print("方法一---计算出二进制数"+ Integer.toBinaryString(num) + "有");
-        int count = 0;
-        for (int i = 0; num > 0; i++) {
-            // 与1相与，判断值是否是1，不是说明这一位是0
-            count += num & 1;
-            num >>= 1;
+        int temp = 1;
+        while(temp <= num) {
+            if (temp == num) {
+                System.out.println(num + "是2的乘方");
+                return ;
+            }
+            temp *= 2;
         }
-        System.out.println(count + "个1");
+        System.out.println(num + "不是2的乘方");
     }
 
     /**
-     *  方法二，就是与(num - 1)相与，当num为0 的时候，循环结束
+     * 方法二，如果一个数n是2的乘方，那么num & (num - 1)的结果是0
+     * @param num 二进制数
      */
     private static void fun2(int num) {
-        System.out.print("方法二---计算出二进制数"+ Integer.toBinaryString(num) + "有");
-        int count = 0;
-        for (int i = 0; num > 0; ++i) {
-            num &= (num - 1);
-            count++;
+        if ((num & (num - 1)) == 0) {
+            System.out.println(num + "是2的乘方");
+        } else {
+            System.out.println(num + "不是2的乘方");
         }
-        System.out.println(count + "个1");
     }
 }
