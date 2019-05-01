379726efaa658f3c28a0a8e788646bd0ece5360d
upupuup <woyaojiayou_a@163.com>
2019-05-01 20:36:07 +0800

    求出两个字符串的相同元素个数

--- src/com/test/CompareDemo.java
+++ src/com/test/CompareDemo.java
@@ -15,9 +15,11 @@ public class CompareDemo {
     }
 
     public static int compared(String strA, String strB) {
+        // 对子串判空
         if (strA != null && strB != null && strA.trim().equals("") && strB.trim().equals("")) {
             return -1;
         }
+
         String min;
         String max;
         if (strA.length() < strB.length()) {
@@ -32,6 +34,7 @@ public class CompareDemo {
             for (int beg = 0, end = min.length() - i; end <= min.length(); beg++, end++) {
                 //取子串
                 String s = min.substring(beg, end);
+
                 //如果大串中含有子串，则返回子串，该子串就是最大的公共子串
                 //这里也可以使用java中String类提供的contains()方法，这里选择自己写一个类似的方法
                 if (max.contains(s)) {
