e949a776129a1d8fc212431dfd01138575533315
upupuup <woyaojiayou_a@163.com>
2020-10-13 22:14:29 +0800

    反射

---
+++ src/com/reflect/Test10.java
@@ -0,0 +1,53 @@
+package com.reflect;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Description: 性能问题
+ * @Author: jiangzhihong
+ * @CreateDate: 2020/10/13 22:09
+ */
+public class Test10 {
+    public static void test01() {
+        User user = new User();
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 10000000; i++) {
+            user.getName();
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("普通方法：" + (endTime - startTime) + "ms");
+
+    }
+
+    public static void test02() throws Exception {
+        User user = new User();
+        Class<? extends User> c1 = user.getClass();
+        Method getName = c1.getMethod("getName", null);
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 10000000; i++) {
+            getName.invoke(user, null);
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("反射方法：" + (endTime - startTime) + "ms");
+    }
+
+    public static void test03() throws Exception {
+        User user = new User();
+        Class<? extends User> c1 = user.getClass();
+        Method getName = c1.getMethod("getName", null);
+        // 关闭权限检查，就是可以获取所以普类型的方法
+        getName.setAccessible(true);
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 10000000; i++) {
+            getName.invoke(user, null);
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("关闭权限：" + (endTime - startTime) + "ms");
+    }
+}
