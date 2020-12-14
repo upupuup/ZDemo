12043f4d9928c82543bc0396a7edeab05b1bd547
jiangzhihong <woyaojiayou_a@163.com>
2020-12-14 19:34:27 +0800

    lambda去除list中有相同用户主键的数据

--- src/com/lambda/LambdaDemo.java
+++ src/com/lambda/LambdaDemo.java
@@ -1,7 +1,8 @@
 package com.lambda;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @Author: jiangzhihong
@@ -34,5 +35,28 @@ public class LambdaDemo {
 		list.stream().map(x -> x * x).forEach(System.out::println);
 		Integer sum = list.stream().map(x -> x * x).reduce((x, y) -> x + y).get();
 		System.out.println("总数：" + sum);
+	}
+
+	private boolean isPrime(int num) {
+		int num2 = (int) Math.sqrt((double) num);
+		return IntStream.rangeClosed(2, num2).noneMatch(t -> num % t == 0);
+	}
+
+	private Map<Boolean, List<Integer>> getNum(int n) {
+		return IntStream.rangeClosed(2, n).boxed().collect(Collectors.partitioningBy(t -> isPrime(t)));
+	}
+
+	/**
+	 * 去除有相同用户主键的数据
+	 */
+	public static void userDemo() {
+		List<User> queryList = new ArrayList<>();
+		// 记得塞数据
+		// 去除有相同用户主键的数据
+		queryList = queryList.stream().collect(Collectors.collectingAndThen(
+				Collectors.toCollection(()->
+						new TreeSet<>(Comparator.comparing(obj -> obj.getUserId()))
+				), ArrayList::new));
+		System.out.println(queryList.toString());
 	}
 }
---
+++ src/com/lambda/User.java
@@ -0,0 +1,35 @@
+package com.lambda;
+
+/*
+ * @Author:         jiangzhihong
+ * @CreateDate:     2020/12/14 19:30
+ */
+public class User {
+	private Long id;
+	private Long userId;
+	private String userName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
